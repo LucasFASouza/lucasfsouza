cbec5be5e7e2044e80c5c8461bd569409e7d67fe
Lucas Souza <outrolucas2002@gmail.com>
2024-06-10 13:15:02 -0300

    Draft portfolio page + minor changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,13 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import I18nSelect from "./I18nSelect";
+import { FormattedMessage, useIntl } from "react-intl";
 
 const Navbar = () => {
   const router = useRouter();
+  const intl = useIntl();
+
+  const links = intl.messages["navbar"] || [];
 
   return (
     <nav className="flex justify-between py-8">
@@ -12,16 +16,13 @@ const Navbar = () => {
         Lucas F. Souza
       </Link>
       <ul className="flex gap-8 items-center">
-        <li>
-          <Link href="/" locale={router.locale}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/resume" locale={router.locale}>
-            Resumé
-          </Link>
-        </li>
+        {links.map((link, index) => (
+          <li key={index}>
+            <Link href={link.href} locale={router.locale}>
+              <span>{link.label}</span>
+            </Link>
+          </li>
+        ))}
         <li>
           <I18nSelect />
         </li>
---
+++ pages/portfolio.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import Modal from "../components/Modal"; // import your Modal component
+import { FormattedMessage, useIntl } from "react-intl";
+import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+const Portfolio = ({ dir }) => {
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const { locales } = useRouter();
+
+  const intl = useIntl();
+  const projects = intl.messages["page.portfolio.projects"] || [];
+
+  const title = intl.formatMessage({ id: "page.home.head.title" });
+  const description = intl.formatMessage({
+    id: "page.home.head.meta.description",
+  });
+
+  const handleOpenModal = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProject(null);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+
+      <main dir={dir}>
+        <Layout locales={locales}>
+          {projects.map((project, index) => (
+            <div className="bg-stone-950 rounded-xl m-4" key={index} onClick={() => handleOpenModal(project)}>
+              <h2>{project.title}</h2>
+              <p>{project["short-description"]}</p>
+              <p>{project.type}</p>
+            </div>
+          ))}
+
+          {selectedProject && (
+            <Modal onClose={handleCloseModal}>
+              <h2>{selectedProject.title}</h2>
+              <p>{selectedProject.description}</p>
+              <h3>Stack</h3>
+              <ul>
+                {selectedProject.stack.map((stack, index) => (
+                  <li key={index}>{stack}</li>
+                ))}
+              </ul>
+              <a href={selectedProject.url}>Link</a>
+            </Modal>
+          )}
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default Portfolio;
